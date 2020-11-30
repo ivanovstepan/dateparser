ce12e87b4f42690e2043355c10627fea68042d51
Stsiapan_Ivanou <ivanou.stsiapan@manomano.com>
2020-11-30 20:20:25 +0300

    add more unit tests and patterns to formats.txt

--- src/test/java/com/ivanov/AppTests.java
+++ src/test/java/com/ivanov/AppTests.java
@@ -12,17 +12,27 @@ public class AppTests {
 
     @Test
     public void positiveTests() {
-        boolean result = adultChecker.isAdult("2000.01.01");
+        boolean result = adultChecker.isAdult("2000.01-01");
         Assert.assertTrue(result);
-        result = adultChecker.isAdult("1999 12 12");
+        result = adultChecker.isAdult("1999+12 12");
         Assert.assertTrue(result);
-        result = adultChecker.isAdult("1888/9.12");
+        result = adultChecker.isAdult("1888^9.12");
         Assert.assertTrue(result);
-        result = adultChecker.isAdult("1488*2*2");
+        result = adultChecker.isAdult("1488:2*2");
         Assert.assertTrue(result);
-        result = adultChecker.isAdult("1458*13*11");
+        result = adultChecker.isAdult("1458#13@11");
         Assert.assertTrue(result);
-        result = adultChecker.isAdult("1658*30*12");
+        result = adultChecker.isAdult("1658!30&12");
+        Assert.assertTrue(result);
+        result = adultChecker.isAdult("2001(07)04 12:08:56");
+        Assert.assertTrue(result);
+        result = adultChecker.isAdult("2001_07=04 12:08:56.235-0700");
+        Assert.assertTrue(result);
+        result = adultChecker.isAdult("2001,W27-3");
+        Assert.assertTrue(result);
+        result = adultChecker.isAdult("02001.July.04 AD 12:08 PM");
+        Assert.assertTrue(result);
+        result = adultChecker.isAdult("0:08 PM,PDT");
         Assert.assertTrue(result);
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.YEAR, -100);
@@ -32,19 +42,27 @@ public class AppTests {
 
     @Test
     public void negativeTests() {
-        boolean result = adultChecker.isAdult("2003.01.01");
+        boolean result = adultChecker.isAdult("2005.01-01");
         Assert.assertFalse(result);
-        result = adultChecker.isAdult("2012 12 12");
+        result = adultChecker.isAdult("2014+1 1");
         Assert.assertFalse(result);
-        result = adultChecker.isAdult("2222/9.12");
+        result = adultChecker.isAdult("2888^9.12");
         Assert.assertFalse(result);
-        result = adultChecker.isAdult("2322*2*2");
+        result = adultChecker.isAdult("2488:2*2");
         Assert.assertFalse(result);
-        result = adultChecker.isAdult("2009*13*11");
+        result = adultChecker.isAdult("2458#13@11");
         Assert.assertFalse(result);
-        result = adultChecker.isAdult("2014*30*12");
+        result = adultChecker.isAdult("2658!30&12");
         Assert.assertFalse(result);
         result = adultChecker.isAdult("ascwevqw");
+        Assert.assertFalse(result);
+        result = adultChecker.isAdult("2011-07-04 12:08:56");
+        Assert.assertFalse(result);
+        result = adultChecker.isAdult("2011-07-04 12:08:56.235-0700");
+        Assert.assertFalse(result);
+        result = adultChecker.isAdult("2011-W27-3");
+        Assert.assertFalse(result);
+        result = adultChecker.isAdult("02011.July.04 AD 12:08 PM");
         Assert.assertFalse(result);
         result = adultChecker.isAdult(new Date());
         Assert.assertFalse(result);
